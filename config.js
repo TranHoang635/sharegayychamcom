1786427bb394239df3fd653c14ef5fe29fcdb4d1
Trần Hoàng <lykhoa2112@gmail.com>
2022-07-06 00:29:11 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Bae à!',
+    introDesc: `Hãy click vào phía dưới để vào tim tui khám phá xem có gì nè 🥰`,
+    btnIntro: 'Gét Gô',
+    title: 'Nhìn vào sách sử cậu thấy được quá khứ 😙',
+    desc: 'Còn nhìn vào tui, cậu thấy được tương lai 🥹',
+    btnYes: 'Tương lai gét gô',
+    btnNo: 'Khum',
+    question:'Trái đất này nhiều đàn ông mà xao lại chọn tui 🤔',
+    btnReply: 'Ấn đây gửi tui nè',
+    reply: 'Yêu mà cũng cần có lý do nữa hả 🥹',
+    mess: 'Ahihi ngại ghê 🥰. Yêu cậu nhiều nhiều nắm nuôn 😘😘',
+    messDesc: 'Tối tui qua rước đi chơi nghe 😘',
+    btnAccept: 'Okiiii',
+    messLink: 'https://m.facebook.com/messages/t/100015629504374' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
